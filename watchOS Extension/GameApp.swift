dac07f17d2a5c999e6f51d408fe33e8b9b769a70
Pavel Sakhanko <iospaveldev@gmail.com>
2021-04-04 21:13:39 +0200

    feat: added watchOS

---
+++ watchOS Extension/GameApp.swift
@@ -0,0 +1,21 @@
+//
+//  GameApp.swift
+//  watchOS Extension
+//
+//  Created by Pavel Sakhanko on 4.04.21.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
---
+++ watchOS Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  watchOS Extension
+//
+//  Created by Pavel Sakhanko on 4.04.21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
